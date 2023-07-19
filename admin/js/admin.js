f6a7226b2456ac96a44576649eefedc80871dd64
Khang <vinhkhang.tl@gmail.com>
2023-07-19 22:01:20 +0700

    sua loi edit khong test masp nua

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -284,19 +284,21 @@ renderListTable()
 getID('admin__modal').onclick = function () {
     thongBao.forEach(value => value.style.display = 'none')
     getID('maSp').readOnly = false
-    getID('maSp').style.backgroundColor = 'none'
+    getID('maSp').style='background-color: none;'
     getID('admin__form').reset()
     getID('imgSpan').src = ''
 }
 getID('saveProduct').onclick = function () {
-    if (nameTest() && maSpTest() && typeTest() && descTest() && screenTest() && backCameraTest() && frontCameraTest() && priceTest()
-    ) {
-        if (getID('idSp').value === '-1') {
+    if (getID('idSp').value === '-1') {
+        if (nameTest() && maSpTest() && typeTest() && descTest() && screenTest() && backCameraTest() && frontCameraTest() && priceTest()) {
             addProduct()
-        } else {
+        }
+    } else {
+        if (nameTest() && typeTest() && descTest() && screenTest() && backCameraTest() && frontCameraTest() && priceTest()) {
             editProduct()
         }
     }
+
 }
 
 
